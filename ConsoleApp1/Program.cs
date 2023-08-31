48aeb30f7b34dc1a485ceebc1b2c91d5b05122af
Henry <Henry@joycastle.mobi>
2023-08-31 19:33:51 +0800

    完成90%

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,113 +1,191 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.IO;
-using System.Net;
-using System.Threading.Tasks;
+using System.Text.Json;
+using Google.Protobuf;
 
-namespace HttpWebRequestQueueWithResume
+
+class Program
 {
-    class Program
+    class Person
     {
-        static void Main(string[] args)
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    static void Main()
+    {
+        string folderPath = "/Users/jc/ExampleFolder";
+
+        if (!Directory.Exists(folderPath))
         {
-            int maxConcurrency = 3;
-            int total = 6;
-            
-            BlockingCollection<string> requestQueue = new BlockingCollection<string>();
-            //AutoResetEvent autoResetEvent = new AutoResetEvent(true);
+            Directory.CreateDirectory(folderPath);
+            Console.WriteLine("文件夹已创建。");
+        }
+        else
+        {
+            Console.WriteLine("文件夹已经存在。");
+            string filePath =  "/Users/jc/ExampleFolder/11111.txt";
 
-            Thread producer = new Thread(() =>
-            {
-                for (int i = 1; i <= total; i++)
-                {
-                    requestQueue.Add($"https://static.runoob.com/images/demo/demo{i}.jpg");
-                }
-                requestQueue.CompleteAdding(); 
-            });
-            
-            Thread[] consumers = new Thread[maxConcurrency];
-            for (int i = 0; i < maxConcurrency; i++)
-            {
-                consumers[i] = new Thread(() =>
-                {
-                    foreach (string url in requestQueue.GetConsumingEnumerable())
-                    {
-                        ProcessRequest(url);
-                    }
-                });
-            }
-            
-            producer.Start();
-            foreach (Thread consumer in consumers)
-            {
-                consumer.Start();
-            }
-            
-            producer.Join();
-            foreach (Thread consumer in consumers)
-            {
-                consumer.Join();
-            }
+            FileInfo fileInfo = new FileInfo(filePath);
+            DirectoryInfo directoryInfo = new DirectoryInfo(folderPath);
 
-            Console.WriteLine("All downloads completed.");
+            Console.WriteLine("文件夹路径: " + fileInfo.DirectoryName);
+            Console.WriteLine("文件名: " + fileInfo.Name);
+            Console.WriteLine("扩展名: " + fileInfo.Extension);
+            Console.WriteLine();
+            
+            Console.WriteLine("文件名: " + fileInfo.Name);
+            Console.WriteLine("文件路径: " + fileInfo.FullName);
+            Console.WriteLine("文件大小: " + fileInfo.Length + " 字节");
+            Console.WriteLine("创建时间: " + fileInfo.CreationTime);
+            Console.WriteLine("最后访问时间: " + fileInfo.LastAccessTime);
+            Console.WriteLine("最后修改时间: " + fileInfo.LastWriteTime);
+            Console.WriteLine();
+            
+            Console.WriteLine("文件夹名称: " + directoryInfo.Name);
+            Console.WriteLine("文件夹路径: " + directoryInfo.FullName);
+            Console.WriteLine("创建时间: " + directoryInfo.CreationTime);
+            Console.WriteLine("最后访问时间: " + directoryInfo.LastAccessTime);
+            Console.WriteLine("最后修改时间: " + directoryInfo.LastWriteTime);
+
+            long size = GetDirectorySize(directoryInfo);
+            Console.WriteLine("文件夹大小: " + size + " 字节");
+            Console.WriteLine();
+
+            WriteFile(filePath);
+            ReadFile(filePath);
+            
+            BinaryWriteFile(filePath);
+            BinaryReadFile(filePath);
+            
+            ReadFile(filePath);
+            
+            WriteJsonFile(filePath);
+            ReadJsonFile(filePath);
+
+            ReadFile(filePath);
+            
+            //WriteProtoBuf(filePath);
+            //ReadProtoBuf(filePath);
+
+        }
+    }
+
+    static long GetDirectorySize(DirectoryInfo directoryInfo)
+    {
+        long size = 0;
+        FileInfo[] infos = directoryInfo.GetFiles();
+        foreach (var info in infos)
+        {
+            size += info.Length;
         }
 
-        static void ProcessRequest(string url)
+        return size;
+    }
+
+    static void WriteFile(string filePath)
+    {
+        using (FileStream fileStream = new FileStream(filePath, FileMode.Append))
         {
-            string fileName = Path.GetFileName(url);
-            long resumePosition = 0;
+            string content = "2222";
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(content);
+            fileStream.Write(bytes, 0, bytes.Length);
+        }
+    }
 
-            if (File.Exists(fileName))
+    static void ReadFile(string filePath)
+    {
+        using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+        {
+            byte[] buffer = new byte[fileStream.Length];
+            int byteRead = fileStream.Read(buffer, 0, buffer.Length);
+            string content = System.Text.Encoding.UTF8.GetString(buffer, 0, byteRead);
+            Console.WriteLine("读取的数据：" + content);
+        }
+    }
+
+    static void BinaryWriteFile(string filePath)
+    {
+        Person person1 = new Person { Name = "A", Age = 30 };
+        Person person2 = new Person { Name = "B", Age = 25 };
+        using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+        {
+            var writer = new BinaryWriter(fileStream);
+            writer.Write(person1.Name);
+            writer.Write(person1.Age);
+
+            writer.Write(person2.Name);
+            writer.Write(person2.Age);
+        }
+    }
+
+    static void BinaryReadFile(string filePath)
+    {
+        using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+        {
+            var reader = new BinaryReader(fileStream);
+            while (reader.BaseStream.Position < reader.BaseStream.Length)
             {
-                FileInfo fileInfo = new FileInfo(fileName);
-                resumePosition = fileInfo.Length;
-            }
+                string name = reader.ReadString();
+                int age = reader.ReadInt32();
 
-            try
-            {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                request.AddRange(resumePosition);
-
-                using (HttpWebResponse response = (HttpWebResponse) request.GetResponse())
-                using (Stream responseStream = response.GetResponseStream())
-                using (FileStream fileStream = new FileStream(fileName, FileMode.Append))
-                {
-                    byte[] buffer = new byte[10240];
-                    int bytesRead;
-
-                    while ((bytesRead = responseStream.Read(buffer, 0, buffer.Length)) > 0)
-                    {
-                        fileStream.Write(buffer, 0, bytesRead);
-                        resumePosition += bytesRead;
-
-                        double progressPercentage = (double)resumePosition / response.ContentLength * 100;
-                        Console.WriteLine(
-                            $"Download Progress for {fileName}: {(int)progressPercentage}% ({resumePosition}/{response.ContentLength} bytes)");
-                    }
-
-                    Console.WriteLine($"HTTP Response Headers for {fileName}:");
-                    foreach (string headerName in response.Headers.AllKeys)
-                    {
-                        Console.WriteLine($"{headerName}: {response.Headers[headerName]}");
-                    }
-
-                    using (StreamReader reader = new StreamReader(responseStream))
-                    {
-                        string responseBody = reader.ReadToEnd();
-                        Console.WriteLine($"HTTP Response Body for {fileName}:\n{responseBody}");
-                    }
-                }
-
-                Console.WriteLine($"Download completed for {fileName}.");
-            }
-            catch (WebException ex)
-            {
-                Console.WriteLine($"WebException for {fileName}: {ex.Message}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception for {fileName}: {ex.Message}");
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("Age: " + age);
+                Console.WriteLine();
             }
         }
     }
+
+    static void WriteJsonFile(string filePath)
+    {
+        Person person1 = new Person { Name = "A", Age = 30 };
+        Person person2 = new Person { Name = "B", Age = 25 };
+
+        JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+
+        string json = JsonSerializer.Serialize(new[] { person1, person2 }, options);
+        File.WriteAllText(filePath, json);
+    }
+
+    static void ReadJsonFile(string filePath)
+    {
+        string json = File.ReadAllText(filePath);
+        Person[] people = JsonSerializer.Deserialize<Person[]>(json);
+
+        foreach (Person person in people)
+        {
+            Console.WriteLine("Name: " + person.Name);
+            Console.WriteLine("Age: " + person.Age);
+        }
+    }
+
+    /*static void WriteProtoBuf(string filePath)
+    {
+        filePath = "people.dat";
+        var person1 = new Person { Name = "C", Age = 30 };
+        var person2 = new Person { Name = "D", Age = 25 };
+
+        using (var output = File.Create(filePath))
+        {
+            Serializer.Serialize(output, person1);
+            Serializer.Serialize(output, person2);
+        }
+    }
+
+    static void ReadProtoBuf(string filePath)
+    {
+        filePath = "people.dat";
+        using (var input = File.OpenRead(filePath))
+        {
+            Person[] people = new Person[2];
+            for (int i = 0; i < 2; i++)
+            {
+                people[i] = Serializer.Deserialize<Person>(input);
+                Console.WriteLine("Name: " + people[i].Name);
+                Console.WriteLine("Age: " + people[i].Age);
+            }
+        }
+    }*/
 }
